e6caf4ce8c2f412339c76b553456ee9712bfb87a
ser08 <sergiomanuel03@gmail.com>
2017-06-24 16:04:06 -0300

    AE R

---
+++ simple_autoencoder.py
@@ -0,0 +1,54 @@
+##  single fully-connected neural layer as encoder and as decoder:
+
+from keras.layers import Input, Dense
+from keras.models import Model
+
+
+from keras.datasets import mnist
+import numpy as np
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print x_train.shape
+print x_test.shape
+
+
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input=input_img, output=decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
+
+
+# create a separate encoder model:
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
+
+
+#decoder model:
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
+
+# configure our model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer:
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+
